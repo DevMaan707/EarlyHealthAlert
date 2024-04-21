2fc18940a73788e3a307a0a0c6c6fa556d12a42c
DevMaan707 <dev.maan707@gmail.com>
2024-04-22 00:18:28 +0530

    basic website

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const server = express();
+const path = require('path');
+const PORT = 8080
+
+server.use(express.static(path.join(__dirname , '/dist')));
+
+server.get('/',(req,res)=>{
+    res.render('index');
+});
+
+server.listen(PORT,()=>{
+console.log(`Server running on port http://localhost:${PORT}`);
+}
+);
